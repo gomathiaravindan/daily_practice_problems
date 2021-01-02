1482752c3983af1c1ed67913e0a690495028415f
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-02 08:28:01 +0530

    Update practice_1.c

--- practice_1.c
+++ practice_1.c
@@ -4,23 +4,18 @@ sample case: n = 13, output: 17
 #include<stdio.h>
 int main()
 {
-	int n,count=0,i;
+	int n,t,count=0,i;
 	printf("Enter n:");
 	scanf("%d",&n);
 	for(i=1;i<=n;i++)
 	{
-		if(i>=1 && i<=9)
+		t = i;
+		while(t!=0)
 		{
 			count++;
+			t/=10;
 		}
-		else if(i>9 && i<=99)
-		{
-			count+=2;
-		}
-		else if(i>99 && i<=999)
-		{
-			count+=3;
-		}
+		printf("The tot number of char:%d ",count);
 	}
-	printf("The tot number of char:%d ",count);
+	
 }
