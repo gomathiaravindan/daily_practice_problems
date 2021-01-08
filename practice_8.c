ca1a0ff2aaa9cf79233befe350888828a25e298b
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-08 19:05:16 +0530

    Add files via upload

---
+++ practice_8.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	int *arr = (int*)calloc(n,sizeof(int));
+	for(i = 0;i< n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		for(j = i+1;j <=n-1;j++)
+		{
+			if(arr[i] < arr[j])
+			{
+				arr[i] = arr[i] + arr[j];
+				arr[j] = arr[i] - arr[j];
+				arr[i] = arr[i] - arr[j];
+			}
+		}
+	}
+	
+	
+		printf("%d ",arr[1]);
+		
+	
+}
