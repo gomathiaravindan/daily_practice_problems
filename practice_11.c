f77f224e81679c84d8f25084f98d28bcfdf1d81a
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-13 14:16:08 +0530

    String is checked for plaindrome.

---
+++ practice_11.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[20];
+	int i,j;
+	gets(s);
+	for(i=0,j=strlen(s)-1;i<j;)
+	{
+		if(s[i]!=s[j])
+		{
+			if(s[i]== ' ')
+			{
+				i++;
+			}
+			else if(s[j]== ' ')
+			{
+				j--;
+			}
+			else
+			{
+				printf("Not a palindrome!!");
+				exit(1);
+			}
+		}
+
+		else if(s[i]==s[j])
+		{
+			i++;j--;
+		}
+	}
+		printf("The given string is palindrome!!");
+	
+	
+	
+}
