b1b274fcf3ca7e8a38dadea6a65baa3d55a84878
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-19 21:52:04 +0530

    Add files via upload

---
+++ practice_14.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,k;
+	scanf("%d",&n);
+	k = n;
+	i = (2 * n) - 1;
+	for(;i>=1;i--)
+	{
+		if(i==n-1)
+		{
+			i = n ;
+			n = n - 1;
+		
+		}
+			
+		else
+		{
+		
+			for(j=0;j<=((2 * n) - 1) - i;j++)
+				{
+					printf("%d",k);
+				}
+			printf("\n");
+		}
+}
+}
