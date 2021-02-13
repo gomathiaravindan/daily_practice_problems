ba03bce215bc2f7ba2a998361661063bafbfb36c
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-02-13 22:58:07 +0530

    Characters are toggled.

---
+++ toggle_case.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char string[30];
+	int i;
+	gets(string);
+	
+	for(i=0;i<strlen(string);i++)
+	{
+		if(string[i] >= 'a' && string[i] <= 'z')
+		{
+			string[i] = string[i] - 32;
+		}
+		else if(string[i] >= 'A' && string[i] <= 'Z')
+		{
+			string[i] = string[i] + 32;
+		}
+	}
+	
+	puts(string);
+}
