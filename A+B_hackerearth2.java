a56fe3e63decb609744024bf67cb79d566f9f967
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-03-22 12:32:47 +0530

    Create A+B_hackerearth2.java

---
+++ A+B_hackerearth2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+import java.math.*;
+
+class TestClass {
+
+            public static void main(String args[] ) throws Exception {
+
+                     Scanner sc = new Scanner(System.in);
+
+                     while(sc.hasNext()){
+
+                                BigInteger a = new BigInteger(sc.next());
+
+                                BigInteger b = new BigInteger(sc.next());
+
+                                 System.out.println(a.add(b));
+
+                      }
+
+               }
+
+}
