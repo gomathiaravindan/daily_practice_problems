04517b51552453d9931d06918a78e5dba9de5666
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-03-31 10:43:13 +0530

    Add files via upload

---
+++ permutation_of_a_string.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+
+void permutation(char *, int,int);
+int main()
+{
+	char str[40];
+	int len,left,right;
+	
+	gets(str);
+	
+	len = strlen(str);
+	left = 0, right = len - 1;
+	
+	permutation(str,left,right);
+}
+
+void permutation(char *str, int left, int right)
+{
+	int i;
+	char temp;
+	
+	if(left == right)
+	{
+		printf("%s\n",str);
+	}
+	
+	else
+	{
+		for(i = left;i<=right;i++)
+		{
+			temp = str[left];
+			str[left] = str[i];
+			str[i] = temp;
+			permutation(str, left+1,right);
+			temp = str[left];
+			str[left] = str[i];
+			str[i] = temp;
+		}
+	}
+}
