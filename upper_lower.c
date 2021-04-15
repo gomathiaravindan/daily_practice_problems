03bd4ce53aab2dc3b7ca7711a120e8b839bbf8a8
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-04-15 19:20:29 +0530

    Add files via upload

---
+++ upper_lower.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[30];
+	int i,ind,upper=0,lower=0,upper_first = 0;
+	
+	gets(str);
+	
+	
+		
+	for(i =1;i<=strlen(str)-1;i++)
+	{
+		
+		 if(str[i] >= 'a' && str[i] <= 'z')
+		{
+			lower++;
+		}
+		else if(str[i] >= 'A' && str[i] <= 'Z')
+		{
+			upper++;
+		}
+	   
+	}
+	if(str[0] >= 'A' && str[i] <= 'Z' && upper == strlen(str) - 1)
+		upper++;
+	
+	else if(str[0] >= 'a' && str[0] <= 'z' && lower == strlen(str) - 1)
+		lower++;
+	else
+		upper_first = 1;
+	
+	if(strlen(str) == upper || strlen(str) == lower || (upper_first == 1 && lower == strlen(str)-1))
+		printf("True");
+	else
+		printf("False");
+}
