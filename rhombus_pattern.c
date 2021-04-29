512772985b0e336a4fd8a6c83f3ab7d521c36f60
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-04-29 20:54:19 +0530

    Add files via upload

---
+++ rhombus_pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("Enter the number of rows:");
+	scanf("%d",&n);
+	
+	for(i = 0;i<n;i++)
+	{
+		for(j = 0;j<n-i;j++)
+		{
+			printf(" ");
+		}
+		for(j = 0;j<n;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
