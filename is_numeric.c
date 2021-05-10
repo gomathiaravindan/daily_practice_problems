0cd4199be02837241da3e9a5ddc417d25611415d
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-05-10 19:06:21 +0530

    Update is_numeric.c

--- is_numeric.c
+++ is_numeric.c
@@ -1,3 +1,7 @@
+## - Validate if a given string is numeric. Examples:"0" => true" 0.1 " => true"abc" => false"1 a" => false"2e10" => trueReturn 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+
+
 #include<stdio.h>
 #include<string.h>
 int main()
