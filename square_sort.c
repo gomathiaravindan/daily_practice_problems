3656e99fa5ef38bd3ae3d3964b5656a9c68245ff
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-05-16 13:46:42 +0530

    Update square_sort.c

--- square_sort.c
+++ square_sort.c
@@ -1,3 +1,10 @@
+## Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing orer in O(N) time complexity.
+Sample Case: Input = [-4,-1,0,3,10]
+Ouput: [0,1,9,16,100]
+
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
@@ -12,34 +19,51 @@ int main()
 		scanf("%d",&arr[i]);
      }
      mid = n/2;
-     square = (int*)malloc(n*sizeof(int));
-     square[k++]  = arr[mid] * arr[mid];
-     
-     for(i = mid-1,j = mid+1;i > 0,j < n;)
-     {
-     	if((arr[i] * arr[i]) > (arr[j] * arr[j]))
-     	{
-     		square[k++] = arr[j] * arr[j];
-     		j++;
-		 }
-		else if((arr[i] * arr[i]) < (arr[j] * arr[j]))
+    square = (int*)malloc(n*sizeof(int));
+    
+    i = 0,j = n - 1;
+    
+    if(((arr[i] * arr[i]) > (arr[mid] * arr[mid])) && ((arr[j] * arr[j]) > arr[mid] * arr[mid]))
+    {
+    	square[0] = arr[mid] * arr[mid];
+    	printf("%d ",square[0]);
+	}
+	else
+	{
+		square[mid] = arr[mid] * arr[mid];
+		
+	}
+	
+		for(i=0,j=n-1;i<j;)
 		{
-			square[k++] = arr[i] * arr[i];
-     		i--;
+			if((arr[i] * arr[i]) < (arr[j] * arr[j]))
+			{
+				square[++k] = arr[j] * arr[j];
+				j--;
+			}
+			else if((arr[i] * arr[i]) > (arr[j] * arr[j]))
+			{
+				square[++k] = arr[i] * arr[i];
+				i++;
+			}
+			else if((arr[i] * arr[i]) == (arr[j] * arr[j]))
+			{
+				square[++k] = arr[i] * arr[i];
+				square[++k] = arr[j] * arr[j];
+				j--;
+				i++;
+			}
 		}
-		else if((arr[i] * arr[i]) == (arr[j] * arr[j]))
+		if(i == 0)
 		{
-			square[k++] = arr[i] * arr[i];
-			square[k++] = arr[j] * arr[j];
-			i--;
-			j++;
+			square[++k] = arr[i] * arr[i];
 		}
-	 }
-	 printf("[");
-	 for(i = 0;i<k;i++)
-	 {
-	 	printf("%d,",square[i]);
-	 }
+	//printf("%d ",k);
+	printf("[");
+    for(i = k;i>0;i--)
+	{
+		printf("%d ",square[i]);
+	 } 
 	 printf("]");
 	 free(arr);
 	 return 0;
