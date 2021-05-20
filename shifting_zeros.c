4ac81005d6708ec431c595dcc9e527d8d5ccc8a6
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-05-20 18:49:54 +0530

    Update shifting_zeros.c

--- shifting_zeros.c
+++ shifting_zeros.c
@@ -1,3 +1,11 @@
+## Given an array, shift the zeroes to the beginning of the array.
+Sample Case: Input = [1, 2, 3, 0, 0, 0, 4, 5]
+Output: [0, 0, 0, 1, 2, 3, 4, 5]
+
+
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
