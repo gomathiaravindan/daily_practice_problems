8114f78e301b7d199f897289a85fa4eff705c258
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-06-03 20:12:45 +0530

    Update maximum_sum_of_subarray.c

--- maximum_sum_of_subarray.c
+++ maximum_sum_of_subarray.c
@@ -1,3 +1,15 @@
+## Find the maximum sum of the subarray.
+We define a k-subsequence of an array as follows:
+1. It is a subsequence of consecutive elements in the array.
+2. the sum of the subsequence;s elements s is evenly divisible by k (i.e s % k == 0)
+Given an integer and input array, findout the number of  k-subsquences. 
+Sample Case:  Input = [1 2 3 4 1], k = 3
+Output: 4 ({1, 2},{1, 2, 3},{2, 3, 4},{3})
+
+
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
